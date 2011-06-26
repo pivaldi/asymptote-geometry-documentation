c566451b26035916bc55ae10ac3bd355454445bd
Philippe Ivaldi <contact@piprime.fr>
2011-06-26 15:31:56 +0200

    Add license to asy-tex.sh

--- bin/asy-tex.sh
+++ bin/asy-tex.sh
@@ -1,4 +1,23 @@
 #! /bin/bash
+
+# Copyright (c) 2011, Philippe Ivaldi <www.piprime.fr>
+# Version: $Id: asy-tex.sh,v 0.0 2011/06/26 15:27:39 Exp $
+# $Last Modified on 2011/06/26
+
+# This program is free software ; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation ; either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY ; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program ; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
 LC_CTYPE=fr_FR.latin-1
 
 ASYCMD="/usr/local/bin/asy"
@@ -48,7 +67,6 @@ sed -r 's/<\/*pre>//g' | sed '1,1d' > "${fic}_tmp" && mv "${fic}_tmp" "$fichtml"
 echo "Conversion en HTML de $ficasy" &&\
     texify
 # fi
-    echo '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@';
 
 # Local variables:
 # coding: latin-1
