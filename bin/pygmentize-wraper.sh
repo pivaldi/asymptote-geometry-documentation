b4fa00044d464423ad3cb047946803fc59b30320
Philippe Ivaldi <contact@piprime.fr>
2011-06-28 00:38:04 +0200

    pygmentyze fully supported for english documentation + add lgpl text + improve makefile

--- bin/pygmentize-wraper.sh
+++ bin/pygmentize-wraper.sh
@@ -3,6 +3,6 @@
 FILE_WITHOUT_EXT=${1%.*}
 [ "$FILE_WITHOUT_EXT" == "" ] && FILE_WITHOUT_EXT=$1
 
-pygmentize -f latex "${FILE_WITHOUT_EXT}.asy" | \
+LC_CTYPE=LATIN1 pygmentize -f latex -O encoding=latin1 -- "${FILE_WITHOUT_EXT}.asy" | \
     sed '/{Verbatim}/d' > "${FILE_WITHOUT_EXT}.tex"
 
