eda66b7e84da6e30eecceb36bef465c13c00e125
Hong-Phuc Bui <hong-phuc.bui@htwsaar.de>
2021-12-07 23:13:21 +0100

    convert encode to UTF-8

--- bin/pygmentize-wraper.sh
+++ bin/pygmentize-wraper.sh
@@ -3,6 +3,6 @@
 FILE_WITHOUT_EXT=${1%.*}
 [ "$FILE_WITHOUT_EXT" == "" ] && FILE_WITHOUT_EXT=$1
 
-LC_CTYPE=LATIN1 pygmentize -f latex -O encoding=latin1 -- "${FILE_WITHOUT_EXT}.asy" | \
+pygmentize -f latex -- "${FILE_WITHOUT_EXT}.asy" | \
     sed '/{Verbatim}/d' > "${FILE_WITHOUT_EXT}.tex"
 
